009d825341766f6994ee05ad73f0e2acda809159
PraiseOmeire <praiseomeire99@gmail.com>
2023-02-07 18:36:34 +0100

    another commit

---
+++ App.js
@@ -0,0 +1,6 @@
+// $(".pic").hover(
+//     function(){
+//             $(".see-more", this).css("display", "block");
+//       }, function(){
+//             $(".see-more", this).css("display", "none");
+//   });
